d89fd30ffb4c44288ef8e4cbd6244faab140fcc3
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-05 07:08:10 -0400

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import './Header.css';
-
+import logo from '../images/Logo.svg'
 export const Header = () => {
 	return (
-		<div>
-			hello
+		<div className='nav'>
+			<img src={logo} alt="" />
+			<div className="link">
+				<a href="">Home</a>
+				<a href="">About</a>
+				<a href="">Shop</a>
+				<a href="">Contact</a>
+			</div>
 		</div>
 	);
 }
