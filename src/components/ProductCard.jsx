7b373de72491231634919e418d8c10f014ed0246
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-06 14:53:42 -0400

    ui modified

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 export const ProductCard = (props) => {
 	const{name,img,price,ratings}=props.product;
-	 const handleAddToCart = props.handleAddToCart;
+	const handleAddToCart = props.handleAddToCart;
 
 
 	return (
